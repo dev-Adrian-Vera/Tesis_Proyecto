6754e59cdbdeebe8310cd86d106115dbbca1edcc
Adrian Vera <89646696+dev-Adrian-Vera@users.noreply.github.com>
2026-02-01 22:51:31 -0500

    Enhance inference history with metadata audio details
    Added metadata to inference history response, including location and coordinates.

--- app/routers/inferencias.py
+++ app/routers/inferencias.py
@@ -173,10 +173,13 @@ def listar_inferencias(
             "log_id": i.log_id,
             "prediccion": i.prediccion_especie,
             "confianza": i.confianza,
-            "top_5": i.top_5,
             "tiempo_ejecucion": i.tiempo_ejecucion,
             "fecha": i.fecha_ejecuta,
-            "usuario": db.query(modelos.Usuario).filter(modelos.Usuario.id_usuario == i.id_usuario).first().nombre_completo if i.id_usuario else "Anónimo"
+            "usuario": db.query(modelos.Usuario).filter(modelos.Usuario.id_usuario == i.id_usuario).first().nombre_completo if i.id_usuario else "Anónimo",
+            "ubicacion": i.meta_audio.localizacion if i.meta_audio else "No disponible",
+            "latitud": i.meta_audio.latitud if i.meta_audio else None,
+            "longitud": i.meta_audio.longitud if i.meta_audio else None,
+            "top_5": i.top_5
         }
         for i in inferencias
     ]
